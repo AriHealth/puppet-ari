14f2f0646d349b0b3220376b05ac81a675a57b4e
Carlos Cavero <carlos.cavero@atos.net>
2018-03-13 17:44:17 +0100

    Include the database creation, user and password outside the db profile

--- manifests/profile/db.pp
+++ manifests/profile/db.pp
@@ -1,19 +1,8 @@
 class profile::db(
-	$root_password,
-	$db,
-	$user,
-	$password
-) {
+	$root_password) {
 # Install mySQL for production environment
   class { '::mysql::server':
     root_password => $root_password,
     remove_default_accounts => true,
-  } ->
-  mysql::db { $db:
-    ensure => present,
-    user     => $user,
-    password => $password,
-    host     => 'localhost',
-	grant    => ['ALL'],
-  }
+  } 
 }
--- manifests/role/hapi.pp
+++ manifests/role/hapi.pp
@@ -1,6 +1,17 @@
-class ari::role::hapi inherits role {
+class ari::role::hapi inherits role (
+	$db,
+	$user,
+	$password
+){
   include profile::java8
   include profile::tomcat8
   include profile::webserver
-  include profile::db
+  include profile::db ->
+  mysql::db { $db:
+    ensure => present,
+    user     => $user,
+    password => $password,
+    host     => 'localhost',
+	grant    => ['ALL'],
+  }
 }
