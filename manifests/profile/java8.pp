3a578a0d610e0b1b1905fd067a9b361bcb0f2616
Carlos Cavero <carlos.cavero@atos.net>
2018-03-13 14:47:19 +0100

    Add the header of the file with the license and use the java module to install it which cover further OS

--- manifests/profile/java8.pp
+++ manifests/profile/java8.pp
@@ -1,21 +1,49 @@
+# === Copyright
+#
+# Copyright (C) 2018  Atos Spain SA. All rights reserved.
+#
+# === License
+#
+# profile::java8 is free software: you can redistribute it and/or modify it under the 
+# terms of the Apache License, Version 2.0 (the License);
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# The software is provided "AS IS", without any warranty of any kind, express or implied,
+# including but not limited to the warranties of merchantability, fitness for a particular
+# purpose and noninfringement, in no event shall the authors or copyright holders be 
+# liable for any claim, damages or other liability, whether in action of contract, tort or
+# otherwise, arising from, out of or in connection with the software or the use or other
+# dealings in the software.
+# 
+# See README file for the full disclaimer information and LICENSE file for full license 
+# information in the project root.
+# 
+# == Class: profile::java8
+#
+# Install Oracle java8 using the puppet module puppet/java version 1.6.0.
+#
+# === Parameters
+#
+# TODO: parameterize the profile.
+#
+# === Variables
+#
+# The variables are set to jdk8 and oracle
+#
+# === Authors
+#
+# Author	Carlos Cavero
+#			Atos Research and Innovation, Atos SPAIN SA
+#			e-mail carlos.cavero@atos.net 
+# 
+
 class profile::java8() {
 
-# Include apt package in the OS
-include apt
-
-Class['apt::update'] -> Package<| |>
-
-  # java  repository
-  apt::ppa {'ppa:webupd8team/java': }
-
-  exec { 'accept-java-license':
-    command => '/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections;/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 seen true | sudo /usr/bin/debconf-set-selections;',
-    before => Package['oracle-java8-installer']
-  } 
-  
-  package { "oracle-java8-installer":
-    ensure => installed,
-    require => Apt::Ppa['ppa:webupd8team/java'],
-  }
+	java::oracle { 'jdk8' :
+	  ensure  => 'present',
+	  version => '8',
+	  java_se => 'jdk',
+	}
 
 }
